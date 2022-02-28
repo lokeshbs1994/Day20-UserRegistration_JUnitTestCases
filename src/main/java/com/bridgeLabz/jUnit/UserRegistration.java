78a7a71d6e25f067530e9760873eb9cf2a7b61e4
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-28 11:08:55 +0530

    ADDED:Checking the last name entered by the user is valid or not using Junit test cases

--- src/main/java/com/bridgeLabz/jUnit/UserRegistration.java
+++ src/main/java/com/bridgeLabz/jUnit/UserRegistration.java
@@ -9,4 +9,10 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
     }
+
+    public boolean lastName(String lastName){
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}+$");
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
 }
